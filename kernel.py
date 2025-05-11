245501d944438b8e1d288aca8bf405f925517713
gregvali <greg.valijan@gmail.com>
2025-05-11 16:36:50 -0700

    Updated Algorithm for PriorityChange Test

--- kernel.py
+++ kernel.py
@@ -38,6 +38,7 @@ class Kernel:
         self.waiting_queue = deque()
         self.idle_pcb = PCB(0)
         self.running = self.idle_pcb
+    
 
     # This method is triggered every time a new process has arrived.
     # new_process is this process's PID.
@@ -46,38 +47,21 @@ class Kernel:
     def new_process_arrived(self, new_process: PID, priority: int) -> PID:
         new_pcb = PCB(new_process, priority)
         self.ready_queue.append(new_pcb)
-        
-        if self.running == self.idle_pcb:
-            self.running = self.choose_next_process()
-            return self.running.pid
-    
-        if self.scheduling_algorithm == "Priority":
-            if new_pcb.priority < self.running.priority or (new_pcb.priority == self.running.priority and new_pcb.pid < self.running.pid):
-                self.ready_queue.append(self.running)
-                self.running = self.idle_pcb
-                self.running = self.choose_next_process()
-                return self.running.pid
-        
-        return self.running.pid
+        return self.choose_next_process().pid
 
 
     # This method is triggered every time the current process performs an exit syscall.
     # DO NOT rename or delete this method. DO NOT change its arguments.
     def syscall_exit(self) -> PID:
         self.running = self.idle_pcb
-        self.running = self.choose_next_process()
-        return self.running.pid
+        return self.choose_next_process().pid
 
 
     # This method is triggered when the currently running process requests to change its priority.
     # DO NOT rename or delete this method. DO NOT change its arguments.
     def syscall_set_priority(self, new_priority: int) -> PID:
         self.running.priority = new_priority
-
-        if self.scheduling_algorithm == "Priority":
-            return self.choose_next_process().pid
-        else:
-            return self.running.pid
+        return self.choose_next_process().pid
 
 
     # This is where you can select the next process to run.
@@ -85,13 +69,18 @@ class Kernel:
     # Feel free to modify this method as you see fit.
     # It is not required to actually use this method but it is recommended.
     def choose_next_process(self):
-      if len(self.ready_queue) == 0:
+            
+        if self.running.pid != 0:                    # if running process is not idle_pcb, append it to front of queue
+            self.ready_queue.appendleft(self.running)
+            
+        if len(self.ready_queue) == 0:               # if the ready queue is still empty, that means there is no process running or waiting -> idle
             return self.idle_pcb
 
-      if self.scheduling_algorithm == "FCFS":
-            return self.ready_queue.popleft() # Take the earliest process in deque and run it
+        if self.scheduling_algorithm == "FCFS":      # Set running process to first in queue if FCFS
+            self.running = self.ready_queue.popleft()
+        else:                                        # If Priority, find best process choice in queue and set it to running process
+            best_process = min(self.ready_queue, key=lambda p: (p.priority, p.pid))
+            self.ready_queue.remove(best_process)
+            self.running = best_process
 
-      elif self.scheduling_algorithm == "Priority":
-            best_process = min(self.ready_queue, key=lambda p: (p.priority, p.pid))  # Find the min priority process in the ready queue
-            self.ready_queue.remove(best_process) # Remove the chosen process from the ready queue
-            return best_process
+        return self.running                          # return new running process
